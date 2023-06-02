8bebe9435766e3bed9b2f2629a1917ed9be097ce
Grant T. Aguinaldo <grant.aguinaldo@gmail.com>
2023-06-02 05:32:50 -0700

    Update print_and_log.jl to handle data type exception

--- src/print_and_log.jl
+++ src/print_and_log.jl
@@ -20,7 +20,7 @@ received this license file.  If not, see <http://www.gnu.org/licenses/>.
 This function takes a message which is one-piece string in julia and print it in console or 
 log file depending on global ```Log``` flag.
 """
-function print_and_log(message::AbstractString)
+function print_and_log(message)
 
     if Log && message isa AbstractString
         println(message)
